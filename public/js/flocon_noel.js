cf7ef0c7b34030640b73463ba14daa33dcc46316
= <guillaume.blas4@gmail.com>
2024-12-12 09:30:44 +0100

    jhjh

--- public/js/flocon_noel.js
+++ public/js/flocon_noel.js
@@ -3,19 +3,17 @@ const snowContainer = document.getElementById("snow");
 function createSnowflake() {
   const snowflake = document.createElement("div");
   snowflake.classList.add("snowflake");
-  snowflake.textContent = "❄"; // Caractère unicode pour un flocon
-  snowflake.style.left = Math.random() * 100 + "vw"; // Position horizontale aléatoire
-  snowflake.style.fontSize = Math.random() * 20 + 10 + "px"; // Taille aléatoire
-  snowflake.style.animationDuration = Math.random() * 5 + 3 + "s"; // Durée de chute aléatoire
+  snowflake.textContent = "❆";
+  snowflake.style.left = Math.random() * 100 + "vw"; 
+  snowflake.style.fontSize = Math.random() * 20 + 10 + "px";
+  snowflake.style.animationDuration = Math.random() * 5 + 3 + "s";
   snowflake.style.opacity = Math.random();
 
   snowContainer.appendChild(snowflake);
 
-  // Supprimer le flocon une fois l'animation terminée
   setTimeout(() => {
     snowflake.remove();
   }, 8000);
 }
 
-// Générer des flocons régulièrement
-setInterval(createSnowflake, 300);
+setInterval(createSnowflake, 50);
