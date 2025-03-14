8198fb1cf79e0ce15c0a09022ad656b53ce21f62
= <guillaume.blas4@gmail.com>
2025-03-14 14:29:57 +0100

    patch burger menu

--- public/js/burgerMenu.js
+++ public/js/burgerMenu.js
@@ -1,22 +1,6 @@
 var burgerMenu = false
 
 function toggleMenu() {
-    const menu = document.querySelectorAll('.div-header ul li');
-    const burger = document.querySelector('.burger-menu');
-    const selected = document.querySelector('.selected');
-    if (!burgerMenu){
-        menu.forEach((item) => {
-        item.style.display = 'flex';
-    });
-    burgerMenu = true;
-    }else{
-        menu.forEach((item) => {
-        item.style.display = 'none';
-    });
-    selected.style.display = 'flex';
-    burgerMenu = false;
-    
-    }
-    
-    
+    const navigation = document.getElementById("nav");
+    for (const e of navigation.children) e.classList.toggle("active");
 }
